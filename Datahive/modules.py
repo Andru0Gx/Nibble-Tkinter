be6ffc6aab998a56383e06385deb1afa0c42f84a
Andru0Gx <Andy20ogx@gmail.com>
2023-08-28 16:30:50 -0400

    Frames Class

--- Datahive/modules.py
+++ Datahive/modules.py
@@ -3,6 +3,14 @@ import customtkinter as ctk # Import the customtkinter module
 
 class Sections (ctk.CTkFrame):
     '''Frames of the app'''
-    def __init__(self, parent, size_x, size_y):
-        super().__init__(master = parent, height=size_y, width=size_x)
-        
+    def __init__(self, parent, size_x, size_y, radius=0, fcolor=None, bcolor='transparent', bg_corner_colors=None, bdcolor = None):
+        super().__init__(
+                master=parent,
+                height=size_y,
+                width=size_x,
+                corner_radius=radius,
+                fg_color=fcolor,
+                bg_color=bcolor,
+                background_corner_colors=bg_corner_colors,
+                border_color=bdcolor
+            )
